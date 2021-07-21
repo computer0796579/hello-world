689c8988f6a545fb42f971d750dc115dc6e77a79
computer0796579 <87704796+computer0796579@users.noreply.github.com>
2021-07-21 05:49:00 -0700

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,6 @@
+class Cat:
+	species = 'Cat'
+	def __init__(self, name):
+		self.name = name
+	def speak(self):
+		print("My name is: " + self.name + ", and I am a " + self.species + " in the Hat.")
